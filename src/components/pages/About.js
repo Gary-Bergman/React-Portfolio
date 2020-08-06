250b3cc6caa5df71c4e67eefeda78cf2aa8332e3
Gary Bergman <garybergman00@gmail.com>
2020-08-06 19:41:31 -0400

    Added headshot to About page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -10,17 +10,17 @@ function About() {
             <legend className="card-title text-primary">About Me</legend>
             <hr className="my-4" />
 
-              <section className="row">
-                <p className="card-text"> <img id="bio-image"
-                  className="col-sm-12 col-md-6 col-lg-4 img-fluid float-left mr-5"
-                  src="assets/images/headshot.jpg" alt="Headshot" />
+            <section className="row">
+              <p className="card-text"> <img id="bio-image"
+                className="col-sm-12 col-md-6 col-lg-4 img-fluid float-left mr-5"
+                src="../assets/images/headshot.jpg" alt="Headshot" />
                 I'm a Full Stack Developer in training based in Philadelphia, PA. I run a small event marketing
                 company, Local Amp LLC, but decided to take up coding to increase my marketable skill sets.
                 Outside of work I love to play Ultimate, video games, board games and guitar! I have only been
                 coding for a few months now, but I love it! I'd like to incorporate creative problem solving
                 into my next junior developer role after graduating from the Penn BootCamp!
                 </p>
-              </section>
+            </section>
           </section>
         </section>
 
