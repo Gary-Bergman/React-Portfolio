8ef8b3950b18ef164cfbd7ef80d67f1949560ed3
Gary Bergman <garybergman00@gmail.com>
2020-08-06 19:10:45 -0400

    Added starter pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import TopNav from "./components/TopNav";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
 
 function App() {
   return (
-    <>
-      <h1>Test</h1>
-    </>
+    <Router>
+      <div>
+        <TopNav />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/pages/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import { Link, Route } from "react-router-dom";
+// import Learn from "./Learn";
+
+function Contact(props) {
+  return (
+    <>
+      <h1>Contact Page</h1>
+    </>
+  );
+}
+
+export default Contact;
