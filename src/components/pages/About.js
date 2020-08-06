a7651d9511a3d77c593b091b57900a07cff53e67
Gary Bergman <garybergman00@gmail.com>
2020-08-06 19:35:13 -0400

    Updated About page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,7 +3,28 @@ import React from "react";
 function About() {
   return (
     <>
-      <h1>About Page</h1>
+      <main className="container pb-3">
+
+        <section className="m-sm-5 p-3 card board aboutMe ">
+          <section className="card-body">
+            <legend className="card-title text-primary">About Me</legend>
+            <hr className="my-4" />
+
+              <section className="row">
+                <p className="card-text"> <img id="bio-image"
+                  className="col-sm-12 col-md-6 col-lg-4 img-fluid float-left mr-5"
+                  src="assets/images/headshot.jpg" alt="Headshot" />
+                I'm a Full Stack Developer in training based in Philadelphia, PA. I run a small event marketing
+                company, Local Amp LLC, but decided to take up coding to increase my marketable skill sets.
+                Outside of work I love to play Ultimate, video games, board games and guitar! I have only been
+                coding for a few months now, but I love it! I'd like to incorporate creative problem solving
+                into my next junior developer role after graduating from the Penn BootCamp!
+                </p>
+              </section>
+          </section>
+        </section>
+
+      </main>
     </>
   );
 }
