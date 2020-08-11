3320a3f5405b587025869fa67ed6435ce1e73a42
Gary Bergman <garybergman00@gmail.com>
2020-08-11 09:50:58 -0400

    Updated router to use proper variable name

--- routes/index.js
+++ routes/index.js
@@ -27,7 +27,7 @@ router.route("/api/submit")
     });
   })
 
-app.get("*", (req, res) => {
+router.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 module.exports = router;
