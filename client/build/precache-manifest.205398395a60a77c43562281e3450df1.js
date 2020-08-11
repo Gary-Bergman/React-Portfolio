2dbd93f4210b8f0e19507e981eddf309ec58e563
Gary Bergman <garybergman00@gmail.com>
2020-08-10 21:00:28 -0400

    ran script build

---
+++ client/build/precache-manifest.205398395a60a77c43562281e3450df1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b45cfed83d27914b323ab2cb6102ec7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad182f77ed66d8b197d9",
+    "url": "/static/css/main.16eb0655.chunk.css"
+  },
+  {
+    "revision": "fd535b24d28a327ac0f4",
+    "url": "/static/js/2.01a0a8ca.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.01a0a8ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad182f77ed66d8b197d9",
+    "url": "/static/js/main.4f3540a8.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
