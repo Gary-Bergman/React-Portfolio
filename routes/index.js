f262a7e1ff7fdf858412a25e1e0cb8d19b2d004b
Gary Bergman <garybergman00@gmail.com>
2020-08-11 09:44:21 -0400

    Updated route to include get for everything other than slash route to go to my build and allows for the root to build out the front end

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,33 @@
 const router = require("express").Router();
 var nodemailer = require('nodemailer');
-require ("dotenv").config()
+require("dotenv").config()
 router.route("/api/submit")
-    .post(function (req,res) {
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: "garybergman00@gmail.com",
-    pass: process.env.GMAIL_PW
-  },
+  .post(function (req, res) {
+    var transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: "garybergman00@gmail.com",
+        pass: process.env.GMAIL_PW
+      },
+    });
+    var mailOptions = {
+      from: req.body.email,
+      to: "garybergman00@gmail.com",
+      subject: 'Portfolio Email From: ' + req.body.name,
+      text: req.body.text
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+        res.send(error);
+      } else {
+        console.log('Email sent: ' + info);
+        res.send(info);
+      }
+    });
+  })
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
-var mailOptions = {
-  from: req.body.email,
-  to: "garybergman00@gmail.com",
-  subject: 'Portfolio Email From: ' + req.body.name,
-  text: req.body.text
-};
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-    res.send(error);
-  } else {
-    console.log('Email sent: ' + info);
-    res.send(info);
-  }
-});
-    })
 module.exports = router;
