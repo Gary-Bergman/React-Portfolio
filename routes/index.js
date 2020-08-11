59e76e169e5492c209ca5139f61fe581206a9e67
Gary Bergman <garybergman00@gmail.com>
2020-08-11 16:28:44 -0400

    Deleted unnecessary folders

--- routes/index.js
+++ routes/index.js
@@ -28,5 +28,7 @@ router.route("/api/submit")
     });
   })
 
-
+router.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 module.exports = router;
--- server.js
+++ server.js
@@ -12,10 +12,6 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
