adaecebb6a14bf9eb4e1fda0b59bd6df2a75c9b1
Gary Bergman <garybergman00@gmail.com>
2020-08-11 17:15:33 -0400

    Added toast to contact submit form

--- client/src/components/pages/Contact.js
+++ client/src/components/pages/Contact.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Notifications, { notify } from 'react-notify-toast';
 import { Card, Container, Form, FormGroup, Label, Input, Button } from "reactstrap";
 import axios from "axios";
 
@@ -10,28 +11,34 @@ function Contact() {
     email: "",
     name: "",
     text: ""
-    
+
   })
 
   const handleInputChange = event => {
     setPostEmail({
       ...postEmail,
-      [event.target.name]:event.target.value
-  })
+      [event.target.name]: event.target.value
+    })
   }
-  
+
   const handleFormSubmit = event => {
     event.preventDefault();
     axios.post("/api/submit", postEmail)
       .then(res => {
         console.log(res)
-    })
+        let myColor = { background: "#007bff", text: "#fffff" }
+        document.getElementById("id1").value = "";
+        document.getElementById("id2").value = "";
+        document.getElementById("id3").value = "";
+        notify.show("Email Sent!", "custom", 4000, myColor)
+      })
   }
 
 
   return (
     <>
       <main>
+        <Notifications />
         <Container className="p-3">
 
           <Card className="m-sm-5 p-3 board">
@@ -40,16 +47,16 @@ function Contact() {
               <hr className="my-4" />
               <FormGroup>
                 <Label for="exampleFormControlInput1">Name</Label>
-                <Input name="name" type="text" className="form-control" id="exampleFormControlInput1" placeholder="John Smith" onChange={handleInputChange} />
+                <Input name="name" type="text" className="form-control" id="id1" placeholder="John Smith" onChange={handleInputChange} />
               </FormGroup>
               <FormGroup>
                 <Label for="exampleFormControlInput1">Email address</Label>
-                <Input name="email" type="email" className="form-control" id="exampleFormControlInput1"
-                  placeholder="example@gmail.com" onChange={handleInputChange}/>
+                <Input name="email" type="email" className="form-control" id="id2"
+                  placeholder="example@gmail.com" onChange={handleInputChange} />
               </FormGroup>
               <FormGroup>
                 <Label for="exampleFormControlTextarea1">Message</Label>
-                <textarea name="text" className="form-control" id="exampleFormControlTextarea1" rows="5" placeholder="Write your text here." onChange={handleInputChange}></textarea>
+                <textarea name="text" className="form-control" id="id3" rows="5" placeholder="Write your text here." onChange={handleInputChange}></textarea>
               </FormGroup>
               <Button type="submit" className="submit" onClick={handleFormSubmit}>Submit</Button>
             </Form>
