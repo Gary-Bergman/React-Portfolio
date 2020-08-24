121b115bb0b694ee97f16db72949acd197f908a9
Gary Bergman <garybergman00@gmail.com>
2020-08-24 14:08:11 -0400

    Updated about section and resume link

--- client/src/components/FooterNav.js
+++ client/src/components/FooterNav.js
@@ -19,11 +19,11 @@ function FooterNav() {
                             <a className="col pl-5" href="https://www.linkedin.com/in/gary-bergman/" target="_blank" rel="noopener noreferrer"><i
                                 className="fa fa-linkedin" aria-hidden="true"></i> </a>
                             {/* <!-- Phone --> */}
-                            <a className="col pl-5"  href="tel:4797151009" target="_blank" rel="noopener noreferrer"><i className="fa fa-phone"
+                            <a className="col pl-5" href="tel:4797151009" target="_blank" rel="noopener noreferrer"><i className="fa fa-phone"
                                 aria-hidden="true"></i> </a>
                             {/* <!-- Resume --> */}
                             <a className="col pl-5"
-                                href="https://drive.google.com/file/d/1HZU6OWLzWQNUQhQZbd7WZihXW6MoJPkH/view?usp=sharing"
+                                href="https://drive.google.com/file/d/1F94wdjv8yIpOelPrx8zTdHZbk0UAFvru/view?usp=sharing"
                                 target="_blank" rel="noopener noreferrer"><i className="fa fa-file-text-o" aria-hidden="true"></i> </a>
 
                         </Row>
--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -16,13 +16,11 @@ function About() {
             <Row>
               <CardText>
                 <img id="bio-image"
-                className="col-sm-12 col-md-6 col-lg-4 img-fluid float-left mr-5"
-                src="../assets/images/headshot.jpg" alt="Headshot" />
-                I'm a Full Stack Developer in training based in Philadelphia, PA. I run a small event marketing
-                company, Local Amp LLC, but decided to take up coding to increase my marketable skill sets.
-                Outside of work I love to play Ultimate, video games, board games and guitar! I have only been
-                coding for a few months now, but I love it! I'd like to incorporate creative problem solving
-                into my next junior developer role after graduating from the Penn BootCamp!
+                  className="col-sm-12 col-md-6 col-lg-4 img-fluid float-left mr-5"
+                  src="../assets/images/headshot.jpg" alt="Headshot" />
+       MERN Stack developer with a background in sales and marketing looking to contribute as a web developer to a JavaScript oriented team. Recent graduate of the intensive, full-time Penn LPS full stack coding bootcamp, with acquired skills including JavaScript, React.js, Node.js, Express.js, MySQL, MongoDB, HTML, CSS, jQuery, Bootstrap, Foundation and Materialize. As a former marketing specialist, I have a knack for framing and design, which translates perfectly to a Jr. Front-End Developer position. I have experiencing designing and building full stack web applications, including working on a four-person team to develop an application, using MVC architecture, to connect parents with teachers in learning pods for small-group tutoring during the Covid-19 pandemic. I was the lead designer for our project, TeachPod, and created the application to be fully mobile-responsive. I’m looking forward to translating my skills to a JavaScript role to build clean, sleek and proficient websites.
+
+
               </CardText>
             </Row>
           </CardBody>
